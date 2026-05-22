ec41be6a474b3f1edc5a6d1377e09aa4298f9326
jishnu <thisisjishnuprasad888@gmail.com>
2026-05-22 14:30:40 +0530

    remove not needed files

--- remove-context-menu.ps1
+++
@@ -1,7 +0,0 @@
-# remove-context-menu.ps1
-# Run as Administrator
-
-Remove-Item -Path "HKCR\.md\shell\Open with Markdown Editor" -Recurse -Force
-Remove-Item -Path "HKCU\Software\Classes\.md" -Recurse -Force
-
-Write-Host "Context menu removed successfully!"
